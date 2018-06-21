ea5006d7490aa8e77146c8f26e77cd8ba4e72912
Greg Zentkovich <gzentkovich@gmail.com>
2018-06-20 17:18:51 -1000

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Lennie',
+    email: 'leanne.graham@aplicable.biz'
+  },
+  {
+    id: 2,
+    name: 'Willie Nickson',
+    username: 'The Willie',
+    email: 'willie99@playitslow.com'
+  },
+  {
+    id: 3,
+    name: 'Katie Kindle',
+    username: 'Kaydee',
+    email: 'katie.kindle555@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Ace McKennah',
+    username: 'Ace',
+    email: 'ace@lendingshrub.biz'
+  },
+  {
+    id: 5,
+    name: 'Kinder Williams',
+    username: 'Akindman',
+    email: 'kinder.williams@wreckingcrew.net'
+  },
+  {
+    id: 6,
+    name: 'Benny Lee',
+    username: 'Bendy',
+    email: 'bendy.lee@obstablebreaker.com'
+  }
+];
