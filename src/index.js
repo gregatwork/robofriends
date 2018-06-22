4b15d32456660aac453a6b7bdf52f1124ff971f1
Greg Zentkovich <gzentkovich@gmail.com>
2018-06-21 17:14:29 -1000

    add search box

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 import './index.css';
-import CardList from './CardList';
-import { robots } from './robots';
+import App from './App';
 
-ReactDOM.render(
-  <CardList robots={ robots }/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
