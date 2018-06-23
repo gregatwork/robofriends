27284d7fe3808c3f4915560719bf4da72ea4c63b
Greg Zentkovich <gzentkovich@gmail.com>
2018-06-22 14:12:52 -1000

    add search feature, and styling

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ class Card extends Component {
     const { id, username, name, email } = this.props;
     return (
       <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow'>
-        <h1>{username}</h1>
+        <h3>{username}</h3>
         <img src={`https://robohash.org/${id}.png?set=set1&size=150x150`} alt='Robot' />
         <div>
           <h2>{name}</h2>
